755f0a8f4f3dd877f1a151c03f73955bbd9a3d22
tanacsg <tanacsg@gmail.com>
2022-06-16 01:23:55 +0200

    remove entity manager instance

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,8 +1,5 @@
 package org.acme;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,9 +7,6 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
-    @Inject
-    EntityManager entityManager;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
