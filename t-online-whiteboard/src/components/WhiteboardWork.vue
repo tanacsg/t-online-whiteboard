1170c3dde9ae61ecccc63f1fc7f914f98a016fa9
tanacsg <tanacsg@gmail.com>
2022-08-16 00:52:30 +0200

    improve list handling

--- t-online-whiteboard/src/components/WhiteboardWork.vue
+++ t-online-whiteboard/src/components/WhiteboardWork.vue
@@ -3,15 +3,27 @@ export default {
   data() {
     return {
       show: true,
-      list: [1, 2, 3]
+      list: [],
+      message: "",
     };
+  },
+  methods: {
+    // Creating function
+    append: function () {
+      if (this.message) {
+        this.list.push(this.message);
+        this.message = "";
+      }
+    },
   },
 };
 </script>
 
 <template>
+  <p>Message is: {{ message }}</p>
+  <input v-model="message" placeholder="New text" required />
   <button @click="show = !show">Toggle List</button>
-  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="append()">Push Message</button>
   <button @click="list.pop()">Pop Number</button>
   <button @click="list.reverse()">Reverse List</button>
 
